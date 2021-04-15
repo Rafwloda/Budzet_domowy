2f1b0621f7eb1cea2bfd284b31202ee16a4aa27e
Rafal <rafal@codium.studio>
2021-04-15 19:33:34 +0200

    test_commit

---
+++ Budzet_domowy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Budzet_domowy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
