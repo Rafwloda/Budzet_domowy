ad3ec530b3d52afb59461d7b0e9ccc138e794d83
unknown <kontakt.filip.rut@example.com>
2021-04-15 20:30:48 +0200

    test2

---
+++ Budzet_domowy/Budget.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Budzet_domowy
+{
+    class Budget
+    {
+
+        public int budget_id;
+        public string budget_name;
+        public string budget_desc;
+        public int cost_ID;
+        public int income_ID;
+
+
+        
+        
+
+    }
+
+
+    
+
+
+}
+
---
+++ Budzet_domowy/Toolbox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Budzet_domowy
+{
+    class Toolbox
+    {
+        public static int addIncome()
+        {
+            return 0;
+        }
+
+
+
+    }
+}
