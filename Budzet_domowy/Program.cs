6a092fe6264e99d458a1492b0a0fdafb3194df6d
unknown <kontakt.filip.rut@example.com>
2021-04-15 19:48:35 +0200

    test

--- Budzet_domowy/Program.cs
+++ Budzet_domowy/Program.cs
@@ -6,7 +6,7 @@ namespace Budzet_domowy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! aa");
         }
     }
 }
